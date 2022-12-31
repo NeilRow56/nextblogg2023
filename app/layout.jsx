afd22c57b289d31a0b845e07ed36444517884d4c
Neil <neilrowland56@yahoo.com>
2022-12-31 10:42:11 +0000

    First commit - layout Navbar Footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
-import './globals.css'
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +10,11 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
